5cb16cd0ce235a3ce00c1b02193597887e019b4f
Ritu Chahar <chaharritu410@gmail.com>
2025-07-25 14:39:02 +0530

    some new testcases

--- src/test/java/TestGit.java
+++ src/test/java/TestGit.java
@@ -1,11 +1,35 @@
+import org.testng.annotations.Test;
 
 public class TestGit {
 
-	public static void main(String[] args) {
+	@Test
+	public void testOne() {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello this is gitstuff"); 
 		System.out.println("Hello this is gitdemo"); 
 	}
 
+	@Test
+	public void testTwoColor() {
+	
+		System.out.println("red");
+		System.out.println("yellow");
+		System.out.println("black");
+		System.out.println("white");
+		System.out.println("green");
+		
+	}
+	
+	@Test
+	public void testTwoFruit() {
+	
+		System.out.println("mango");
+		System.out.println("apple");
+		System.out.println("banana");
+	
+		
+	}
+	
+	
 }
